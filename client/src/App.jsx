24d7157119a5627fa468582f0c481f53952ded38
tsumitakaoru <k.tsumita01@gmail.com>
2022-02-03 20:06:26 -0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import useApplicationData from './hooks/useApplicationData';
+
+export default function App() {
+  
+  return (<div className="App" >
+    <h1> App </h1>
+  </div >
+  );
+}
