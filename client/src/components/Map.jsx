ce78acae7c185a2164c3513ffabab3649424a34c
tsumitakaoru <k.tsumita01@gmail.com>
2022-02-10 15:18:49 -0500

    Add Marker

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import mapStyles from '../mapStyles';
 
+const mapContainerStyle = {
+  width: '550px',
+  height: '300px'
+};
+const center = {
+  lat: 43.654225,
+  lng: -79.383186
+}
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+}
+const libraries = ["places"];
 
 function Map() {
-  const libraries = ["places"];
-  const mapContainerStyle = {
-    width: '550px',
-    height: '300px'
-  };
-  const center = {
-    lat: 43.654225,
-    lng: -79.383186
-  }
-  const options = {
-    styles: mapStyles,
-    disableDefaultUI: true,
-    zoomControl: true,
-  }
-
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
+  const [markers, setMarkers] = useState([]);
 
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
@@ -33,7 +33,26 @@ function Map() {
       center={center}
       zoom={8}
       options={options}
-    ></GoogleMap>
+      onClick={(event) => {
+        // console.log(event);
+        setMarkers((current) => [
+          ...current,
+          {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+            time: new Date(),
+          },
+        ]);
+      }
+    }
+    >
+      {markers.map(marker => ( 
+        <Marker
+          key={marker.time.toISOString}
+          position={{ lat: marker.lat, lng: marker.lng }}
+        />
+      ))}
+    </GoogleMap>
   );
 }
 
