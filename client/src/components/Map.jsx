a7b505aec81235da40b542a6c545035bd85f2695
tsumitakaoru <k.tsumita01@gmail.com>
2022-02-10 16:14:56 -0500

    Add maker icon

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import mapStyles from '../mapStyles';
 
@@ -23,6 +23,18 @@ function Map() {
     libraries,
   });
   const [markers, setMarkers] = useState([]);
+  const onMapClick = useCallback((event) => {
+    // console.log(event);
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, [])
+
 
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
@@ -33,23 +45,18 @@ function Map() {
       center={center}
       zoom={8}
       options={options}
-      onClick={(event) => {
-        // console.log(event);
-        setMarkers((current) => [
-          ...current,
-          {
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng(),
-            time: new Date(),
-          },
-        ]);
-      }
-    }
+      onClick={onMapClick}
     >
       {markers.map(marker => ( 
         <Marker
           key={marker.time.toISOString}
           position={{ lat: marker.lat, lng: marker.lng }}
+          icon={{
+            url: '/books.png',
+            scaledSize: new window.google.maps.Size(30, 30),
+            origin: new window.google.maps.Point(0, 0),
+            anchor: new window.google.maps.Point(15, 15),
+          }}
         />
       ))}
     </GoogleMap>
