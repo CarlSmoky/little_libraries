c491921624d394e4339ff22043cd6fee013007a3
tsumitakaoru <k.tsumita01@gmail.com>
2022-02-12 17:34:37 -0500

    Modulize Search

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,18 +1,10 @@
 import React, { useState, useCallback, useRef } from 'react'
-import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
-import mapStyles from '../mapStyles';
-import {formatRelative} from "date-fns";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
+import { GoogleMap, useLoadScript, Marker, InfoWindow, Data } from '@react-google-maps/api';
+import mapStyles from './mapStyles';
+import './Map.css';
+import { formatRelative } from "date-fns";
+import Search from './Search';
+
 
 const mapContainerStyle = {
   width: '550px',
@@ -28,6 +20,8 @@ const options = {
   zoomControl: true,
 }
 const libraries = ["places"];
+
+
 
 function Map() {
   const { isLoaded, loadError } = useLoadScript({
@@ -54,50 +48,60 @@ function Map() {
     mapRef.current = map;
   }, []);
 
+  const panTo = useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
 
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
 
-  return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      center={center}
-      zoom={8}
-      options={options}
-      onClick={onMapClick}
-      onLoad={onMapLoad}
-    >
-      {markers.map(marker => (
-        <Marker
-          key={marker.time.toISOString}
-          position={{ lat: marker.lat, lng: marker.lng }}
-          icon={{
-            url: '/books.png',
-            scaledSize: new window.google.maps.Size(30, 30),
-            origin: new window.google.maps.Point(0, 0),
-            anchor: new window.google.maps.Point(15, 15),
-          }}
-          onClick={() => {
-            setSelected(marker);
-          }}
-        />
-      ))}
+  
 
-      {selected ? (
-        <InfoWindow 
-          position={{lat: selected.lat, lng: selected.lng}}
-          onCloseClick={() => {
-            setSelected(null);
-          }}
-        >
-          <div>
-            <h2>Little Library</h2>
-            <p>Spotted {formatRelative(selected.time, new Date())}</p>
-          </div>
-        </InfoWindow>
+
+  return (
+    <>
+      <Search panTo={panTo}/>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        zoom={8}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map(marker => (
+          <Marker
+            key={marker.time.toISOString}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: '/books.png',
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>Little Library</h2>
+              <p>Spotted {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
         ) : null}
-    </GoogleMap>
+      </GoogleMap>
+    </>
   );
 }
-
 export default Map;
