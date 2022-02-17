22648a008971b14832eea617b410e99f03aec27c
tsumitakaoru <k.tsumita01@gmail.com>
2022-02-17 15:19:46 -0500

    Add login route on server

---
+++ server/routes/login.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+
+  router.get('/', (req, res) => {
+    res.send({ title: 'login' });
+
+  });
+
+  return router;
+};
