698befb20f0c504fbc44834b4458dc68b14d32be
tsumitakaoru <k.tsumita01@gmail.com>
2022-02-18 18:50:14 -0500

    Change login route on server

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,11 +2,40 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 
-module.exports = (db) => {
+module.exports = ({getUserByEmail}) => {
 
-  router.get('/', (req, res) => {
-    res.send({ title: 'login' });
+  router.post('/', (req, res) => {
+    getUserByEmail(req.body.email)
+      .then(user => {
+        if (!user) {
+          res
+            .status(401)
+            .send({ message: 'This username is not registered.' });
+          return;
+        }
 
+        bcrypt.compare(req.body.password, user.password)
+          .then(result => {
+            if (result) {
+              res
+                .status(200)
+                .send({
+                  message: "Login Success!",
+                  id: user.id,
+                  firstName: user.first_name,
+                  lastName: user.last_name,
+                  phoneNumber: user.phone_number,
+                  email: user.email
+                });
+            } else {
+              res
+                .status(401)
+                .send({ message: "Entry password invalid. Try again!" });
+            }
+          });
+      }).catch(err => {
+        console.log(err);
+      });
   });
 
   return router;
