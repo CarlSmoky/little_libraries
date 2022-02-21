66f2c2d2a11109a8ec7a1a8f124959aa93d91f1a
tsumitakaoru <k.tsumita01@gmail.com>
2022-02-21 11:52:46 -0500

    Add libraries table

---
+++ server/db/seeds/02_libraries.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+libraries(address, lat, long, image_url)
+VALUES
+    ('2174 Danforth Ave', 43.686450,-79.310410,'https://firebasestorage.googleapis.com/v0/b/little-libraries-ea3cb.appspot.com/o/images%2Fsample_library.jpg?alt=media&token=35b1e831-d995-4ec2-993b-13f5e5d49c18');
