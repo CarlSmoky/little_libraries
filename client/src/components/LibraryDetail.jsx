535241797cbdcfebbd88840af050af9c6aed893a
tsumitakaoru <k.tsumita01@gmail.com>
2022-03-11 14:35:44 -0500

    Add LibraryDetail.jsx

---
+++ client/src/components/LibraryDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LibraryDetail = ({libraryInfo}) => {
+  return (
+    <div>
+      <h1>LibraryDetail</h1>
+      <p>name:{libraryInfo.address}</p>
+      <p>description:</p>
+    </div>
+  )
+}
+
+export default LibraryDetail;
