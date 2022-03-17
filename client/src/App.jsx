f81532890c0f4aa092467a4ba5d2ed04560930e6
tsumitakaoru <k.tsumita01@gmail.com>
2022-03-17 14:14:03 -0400

    Change / route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import useApplicationData from './hooks/useApplicationData';
-import Home from './components/Home';
+import HomePage from './pages/HomePage';
 import TopNav from './components/TopNav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
-import Map from './components/Map';
 import ImageLoadTest from './components/ImageLoadTest';
 import ImageDownloadTest from './components/ImageDownloadTest';
 import LibraryForm from './components/LibraryForm';
@@ -17,11 +16,8 @@ export default function App() {
   return (<div className="App" >
     <BrowserRouter>
       <TopNav />
-      <Home />
-      {/* <SignUp /> */}
-      {/* <Login /> */}
       <Routes>
-        <Route path ="/" element={<Map />}/>
+        <Route path ="/" element={<HomePage />}/>
         <Route path ="/signup" element={<SignUp />}/>
         <Route path ="/login" element={<Login />}/>
         <Route path ="/upload" element={<ImageLoadTest />}/>
