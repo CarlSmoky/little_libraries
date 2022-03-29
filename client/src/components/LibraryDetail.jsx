a3dd843bda5676a9dcfa2fd05a453bd392dfa105
tsumitakaoru <k.tsumita01@gmail.com>
2022-03-28 20:06:27 -0400

    Add libraryId

--- client/src/components/LibraryDetail.jsx
+++ client/src/components/LibraryDetail.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import { getStorage, ref, getDownloadURL } from "firebase/storage"; // temp
 
 
-const LibraryDetail = ({libraryInfo}) => {
+const LibraryDetail = ({ libraryInfo }) => {
   const [selectedImageUrl, setSelectedImageUrl] = useState();
   const storage = getStorage();
+  const libraryId = libraryInfo.id;
 
   if (libraryInfo.id) {
     getDownloadURL(ref(storage, `images/${libraryInfo.id}.jpg`))
-        .then(url => {
-          setSelectedImageUrl(url);
-    })
+      .then(url => {
+        setSelectedImageUrl(url);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   return (
     <div className="libraryDetails">
-      {selectedImageUrl && <img src={selectedImageUrl} alt="photo of library"/>}
+      {selectedImageUrl && <img src={selectedImageUrl} alt="photo of library" />}
       <p>{libraryInfo.address}</p>
+      {!selectedImageUrl && <Link to="/upload" state={{libraryId}}>Upload image</Link>}
     </div>
   )
 }
