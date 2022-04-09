8834faf49e26464b12b14329cc54a29d8919bc23
tsumitakaoru <k.tsumita01@gmail.com>
2022-04-09 14:12:01 -0400

    Add Jwt to visit

--- server/routes/visits.js
+++ server/routes/visits.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const verifyJWT = require('./verifyJWT');
-const {
-  getVisitCountByLibrary,
-  getVisitCountByUser,
-  recordVisit
-} = require('../helpers/dataHelpers');
 
 module.exports = ({
   getVisitCountByLibrary,
-  getVisitCountByUser,
   recordVisit
 }) => {
   /* GET visit count. */
@@ -22,9 +16,9 @@ module.exports = ({
       }));
   });
 
-  router.post('/', (req, res) => {
-    const { userId, libraryId } = req.body;
-    // console.log("HERE ->", req.user.id);
+  router.post('/', verifyJWT, (req, res) => {
+    const { libraryId } = req.body;
+    const userId = req.user.id;
     recordVisit(userId, libraryId)
       .then((count) => res.json(count))
       .catch((err) => res.json({
