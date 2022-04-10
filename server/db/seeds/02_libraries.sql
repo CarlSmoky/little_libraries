30993d8491f244d1c39a8cd94caaaf671faea16e
tsumitakaoru <k.tsumita01@gmail.com>
2022-04-10 11:39:22 -0400

    Remove client

--- server/db/seeds/02_libraries.sql
+++ server/db/seeds/02_libraries.sql
@@ -1,9 +1,4 @@
 INSERT INTO
 libraries(address, lat, long)
 VALUES
-    ('2174 Danforth Ave', 43.686450,-79.310410),
-    ('951 Woodbine Ave', 43.6851808, -79.3125685),
-    ('2234 Queen St E, Toronto, ON M4E 1L8, Canada', 43.6728117, -79.2874613),
-    ('20 Bellefair Ave', 43.6711894, -79.3004939),
-    ('304 Lee Ave', 43.6766954, -79.3008028),
-    ('72 Brookside Dr',43.6779990, -79.3052306);
+    ('2174 Danforth Ave', 43.686450,-79.310410);
