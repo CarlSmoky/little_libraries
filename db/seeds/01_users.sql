ba5767c3823b031a21d2b1714c737ac4e4cc0b38
tsumitakaoru <k.tsumita01@gmail.com>
2022-04-11 17:17:27 -0400

    migrated db/seeds/01_users.sql

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,9 +1,9 @@
 INSERT INTO
-users(first_name, last_name, phone_number, email, password)
+users(first_name, last_name, email, password)
 VALUES
-    ('Raymond', 'Holt','416-555-0101','cheddar@nypd99.com', '$2a$10$cg8yVVU67gHHySgHJVRAgO4MU0j5mVv.KjNAjH4YQjs7/t8u3PrBK'),
-    ('Jake', 'Peralta', '647-578-0167','coolcoolcool@gmail.com', '$2a$10$Gw9KeuS71Y36HwKrCZ6Xq.dxGAb87.ubMxW3mN8vZyiNWpcZcbuA2'),
-    ('Amy', 'Santiago', '416-874-0107','hallmonitor@gmail.com', '$2a$10$1KWUUBKCvlwZzqlNATF1we4cMgqP.iNNs.5kn.AuK/jS4EAQWeALC'),
-    ('Charles','Boyle', '443-487-1111','charles.boyle@gmail.com', '$2a$10$WsEFmx4oXuMZ3nSGrLMn0eFAxMRelbENrALGnP1eY99wCr5CtMpMK'),
-    ('Rosa','Diaz','416-778-7778','ro-ro@gmail.com', '$2a$10$BNmEZ6lSX/wH4kryFS1a7ehsB2FRuZwiIaEJlcvUp0aHD88IrMr9y'),
-    ('Terry','Jeffords','416-465-0248','ebonyfalcon@gmail.com', '$2a$10$Y3dojvQggIYuSCGhDBBrBONxc4ZJ4Vzf5/EcDozU1PvdBK/Fs.6aK')
+    ('Raymond', 'Holt', 'cheddar@nypd99.com', '$2a$10$cg8yVVU67gHHySgHJVRAgO4MU0j5mVv.KjNAjH4YQjs7/t8u3PrBK'),
+    ('Jake', 'Peralta', 'coolcoolcool@gmail.com', '$2a$10$Gw9KeuS71Y36HwKrCZ6Xq.dxGAb87.ubMxW3mN8vZyiNWpcZcbuA2'),
+    ('Amy', 'Santiago', 'hallmonitor@gmail.com', '$2a$10$1KWUUBKCvlwZzqlNATF1we4cMgqP.iNNs.5kn.AuK/jS4EAQWeALC'),
+    ('Charles','Boyle', 'charles.boyle@gmail.com', '$2a$10$WsEFmx4oXuMZ3nSGrLMn0eFAxMRelbENrALGnP1eY99wCr5CtMpMK'),
+    ('Rosa','Diaz','ro-ro@gmail.com', '$2a$10$BNmEZ6lSX/wH4kryFS1a7ehsB2FRuZwiIaEJlcvUp0aHD88IrMr9y'),
+    ('Terry','Jeffords','ebonyfalcon@gmail.com', '$2a$10$Y3dojvQggIYuSCGhDBBrBONxc4ZJ4Vzf5/EcDozU1PvdBK/Fs.6aK')
