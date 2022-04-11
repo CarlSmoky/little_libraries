f62aeff2ceb7bfcdffb58c6cdd039c155c97ab8b
tsumitakaoru <k.tsumita01@gmail.com>
2022-04-10 20:01:13 -0400

    migrates db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,16 @@
 const pg = require('pg');
 require('dotenv').config();
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const localConnectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const productionConnectionString = process.env.DATABASE_URL;
+const connectionString = (process.env.NODE_ENV === 'local') ? localConnectionString : productionConnectionString;
 
 const client = new pg.Client({
-  connectionString: connectionString || process.env.DATABASE_URL,
+  connectionString: connectionString,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
-console.log(`Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}`);
+console.log(`connected to ${connectionString}, in environment: ${process.env.NODE_ENV}`);
 client.connect();
 
 module.exports = client;
