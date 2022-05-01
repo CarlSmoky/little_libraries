0cf57ffaeff3f560be000178b71941c8c492c301
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-01 15:34:05 -0400

    Clean code

--- routes/verifyJWT.js
+++ routes/verifyJWT.js
@@ -15,9 +15,7 @@ const verifyJWT = (req, res, next) => {
       if (err) {
         res.json({ auth: false, message: "U failed to authenticate" });
       } else {
-        // req.userID = decoded.id;
         req.user = decoded; //This will pass to next destination, you can use req.user
-        console.log(req.user);
         next();
       }
     });
